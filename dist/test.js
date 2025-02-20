9f66810544a802469bec279b8bf7ff51dfd4fda3
kian1991 <kianluetke@gmail.com>
2025-02-20 13:08:09 +0100

    Remove deprecated database and routing files; update GitHub Actions workflow for artifact zipping

--- dist/test.js
+++
@@ -1,9 +0,0 @@
-import { CompoundModel } from './models/compound.js';
-import 'dotenv/config';
-async function main() {
-    await CompoundModel.partialUpdate(1, {
-        groesse: 200,
-        instandhaltungskosten: 220,
-    });
-}
-main();
